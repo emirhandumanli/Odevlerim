aa88b2ebb70211a2475887a4d875d38ce7e86bef
emirhandumanli <emirhandumanli7@gmail.com>
2021-11-05 17:17:58 +0300

    Initial commit

---
+++ src/com/medipol/odevler/EvOdevlerim.java
@@ -0,0 +1,10 @@
+package com.medipol.odevler;
+
+class EvOdevlerim {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/medipol/odevlerim/toplamaodev.java
@@ -0,0 +1,10 @@
+package com.medipol.odevlerim;
+
+public class toplamaodev {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
